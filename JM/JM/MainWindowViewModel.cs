96503ddfb2eb66d72643d3321925e9f4f8187920
DESKTOP-PEPNLPD\pawel <paweltruong@gmail.com>
2018-10-02 19:13:53 +0200

    po mergu

---
+++ JM/JM/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace JM
+{
+    public class MainWindowViewModel : BindableBase
+    {
+    }
+}
+
